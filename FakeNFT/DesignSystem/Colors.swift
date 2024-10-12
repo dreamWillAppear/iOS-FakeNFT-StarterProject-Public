85b5b890ed6305049198463797d4f287cc319d83
dreamlogin <dreamlogin@vk.team>
2024-10-12 23:17:17 +0300

    Добавлены цвета согласно макету, добавленные цвета внесены extension UIColor. Добавлена AppIcon согласно макету.

--- FakeNFT/DesignSystem/Colors.swift
+++ FakeNFT/DesignSystem/Colors.swift
@@ -25,43 +25,30 @@ extension UIColor {
         )
     }
 
-    // Ниже приведены примеры цветов, настоящие цвета надо взять из фигмы
-
-    // Primary Colors
-    static let primary = UIColor(red: 0 / 255, green: 122 / 255, blue: 255 / 255, alpha: 1.0)
-
-    // Secondary Colors
-    static let secondary = UIColor(red: 255 / 255, green: 193 / 255, blue: 7 / 255, alpha: 1.0)
-
-    // Background Colors
-    static let background = UIColor.white
-
     // Text Colors
-    static let textPrimary = UIColor.black
-    static let textSecondary = UIColor.gray
-    static let textOnPrimary = UIColor.white
-    static let textOnSecondary = UIColor.black
+    static let ypTextPrimary = UIColor.ypBlack
+    static let ypTextSecondary = UIColor.ypGreyUniversal
+    static let ypTextOnPrimary = UIColor.ypWhite
+    static let ypTextOnSecondary = UIColor.ypBlack
 
-    private static let yaBlackLight = UIColor(hexString: "1A1B22")
-    private static let yaBlackDark = UIColor.white
-    private static let yaLightGrayLight = UIColor(hexString: "#F7F7F8")
-    private static let yaLightGrayDark = UIColor(hexString: "#2C2C2E")
+    static let ypWhite = UIColor(named: "YPWhite")
+    static let ypBlack = UIColor(named: "YPBlack")
+    static let ypLightGrey = UIColor(named: "YPLightGrey")
+    static let ypWhiteUniversal = UIColor(named: "YPWhiteUniversal")
+    static let ypBlackUniversal = UIColor(named: "YPBlackUniversal")
+    static let ypBackgroundUniversal = UIColor(named: "YPBackgroundUniversal")
+    static let ypBlueUniversal = UIColor(named: "YPBlueUniversal")
+    static let ypGreyUniversal = UIColor(named: "YPGreyUniversal")
+    static let ypGreenUniversal = UIColor(named: "YPGreenUniversal")
+    static let ypRedUniversal = UIColor(named: "YPRedUniversal")
+    static let ypYellowUniversal = UIColor(named: "YPYellowUniversal")
+    
+    static let segmentActive = UIColor.ypBlack
 
-    static let segmentActive = UIColor { traits in
-        return traits.userInterfaceStyle == .dark
-        ? .yaBlackDark
-        : .yaBlackLight
-    }
+    static let segmentInactive = UIColor.ypLightGrey
 
-    static let segmentInactive = UIColor { traits in
-        return traits.userInterfaceStyle == .dark
-        ? .yaLightGrayDark
-        : .yaLightGrayLight
-    }
-
-    static let closeButton = UIColor { traits in
-        return traits.userInterfaceStyle == .dark
-        ? .yaBlackDark
-        : .yaBlackLight
-    }
+    static let closeButton = UIColor.ypBlack
+    
 }
+
+

9b975db69a0d0f56533a9301d06d350318423ae7
folawer21 <folawer21@mail.ru>
2024-10-14 16:23:20 +0300

    added profile and statistics screens done 1/3 statistics

--- FakeNFT/Scenes /TabBarController/TabBarController.swift
+++ FakeNFT/Scenes /TabBarController/TabBarController.swift
@@ -9,16 +9,13 @@ final class TabBarController: UITabBarController {
         image: UIImage(systemName: "square.stack.3d.up.fill"),
         tag: 0
     )
-
     private let statisticTabBarItem = UITabBarItem(title: NSLocalizedString("Tab.statistic", comment: ""), image: UIImage(systemName: "flag.2.crossed.fill"), tag: 1)
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         let catalogController = TestCatalogViewController(
             servicesAssembly: servicesAssembly
         )
         catalogController.tabBarItem = catalogTabBarItem
-        
         
         let statisticVC = RatingTableViewController()
         let navStatisticVC = UINavigationController(rootViewController: statisticVC)

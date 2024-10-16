96c73224c80c287c5ef9f1b720f3b6d2b39548fe
kostyanTs <strong.coy@gmail.com>
2024-10-16 17:45:20 +0500

    Cверстал ячейку коллекции на экране оплаты

---
+++ FakeNFT/Scenes /Cart/CollectionViewCells/PaymentCollectionViewCell.swift
@@ -0,0 +1,89 @@
+//
+//  PaymentCollectionViewCell.swift
+//  FakeNFT
+//
+//  Created by Konstantin on 16.10.2024.
+//
+
+import UIKit
+
+final class PaymentCollectionViewCell: UICollectionViewCell {
+       
+    lazy var containerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .ypLightGrey
+        view.clipsToBounds = true
+        view.layer.cornerRadius = 12
+        return view
+    }()
+    
+    lazy var paymentImage: UIImageView = {
+        let imageVIew = UIImageView()
+        return imageVIew
+    }()
+    
+    lazy var paymentName: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 13, weight: .regular)
+        label.textColor = .ypBlack
+        return label
+    }()
+    
+    lazy var paymentShortName: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 13, weight: .regular)
+        label.textColor = .ypGreenUniversal
+        return label
+    }()
+    
+    static let identifier = "PaymentCell"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+//    override func draw(_ rect: CGRect) {
+//        super.draw(rect)
+//        emojiViewContainer.layer.cornerRadius = emojiViewContainer.frame.size.width / 2
+//        plusButton.layer.cornerRadius = plusButton.frame.size.width / 2
+//    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    private func setupViews() {
+        
+        [containerView].forEach{
+            contentView.addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        [paymentImage,
+         paymentName,
+         paymentShortName].forEach{
+            containerView.addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        NSLayoutConstraint.activate([
+            containerView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 0),
+            containerView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 0),
+            containerView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: 0),
+            containerView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: 0),
+            
+            paymentImage.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 5),
+            paymentImage.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 12),
+            paymentImage.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -5),
+            paymentImage.widthAnchor.constraint(equalTo: paymentImage.heightAnchor),
+            
+            paymentName.leadingAnchor.constraint(equalTo: paymentImage.trailingAnchor, constant: 4),
+            paymentName.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 5),
+            
+            paymentShortName.leadingAnchor.constraint(equalTo: paymentImage.trailingAnchor, constant: 4),
+            paymentShortName.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -5),
+            
+        ])
+    }
+}

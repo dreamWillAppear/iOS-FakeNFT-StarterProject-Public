eccc77cb5fa7421ab0fd000c56e5339a67a2de14
dreamlogin <dreamlogin@vk.team>
2024-10-17 01:30:45 +0300

    Предварительно отстроил raitingStackView для отображения рейтинга в ячейке, реализовал метод отображения рейтинга по переданному рейтингу от 0 до 5

--- FakeNFT/Scenes /Catalog/NftCollection/NftCollectionViewCell.swift
+++ FakeNFT/Scenes /Catalog/NftCollection/NftCollectionViewCell.swift
@@ -8,7 +8,7 @@ final class NftCollectionViewCell: UICollectionViewCell {
     
     // MARK: - Private Properties
     
-    private lazy var cover: UIImageView = {
+    private lazy var coverImageView: UIImageView = {
         let cover = UIImageView()
         cover.layer.masksToBounds = true
         cover.layer.cornerRadius = 12
@@ -26,6 +26,16 @@ final class NftCollectionViewCell: UICollectionViewCell {
         let stackView = UIStackView()
         stackView.axis = .horizontal
         stackView.spacing = 0.75
+        
+        let whiteStarImage = UIImage(systemName: "star.fill")?.withTintColor(.ypLightGrey ?? .lightGray, renderingMode: .alwaysOriginal)
+        let starImageView = UIImageView()
+        starImageView.image = whiteStarImage
+        let starsArrayImageView = Array(repeating: starImageView, count: 5)
+        
+        starsArrayImageView.forEach {
+            stackView.addArrangedSubview($0)
+        }
+        
         return stackView
     }()
     
@@ -58,11 +68,20 @@ final class NftCollectionViewCell: UICollectionViewCell {
         fatalError("init(coder:) has not been implemented")
     }
     
+    //MARK: - Public Methods
+    
+    private func configureCell(cover: UIImage, name: String, isLiked: Bool, raitng: Int, price: Float, isInCart: Bool) {
+
+        
+    }
+    
     // MARK: - Private Properties
     
     private func setupUI() {
         
-        let views = [cover, likeButton, raitingStackView, nameLabel, priceLabel, cartButton]
+        contentView.backgroundColor = .ypWhite
+        
+        let views = [coverImageView, likeButton, raitingStackView, nameLabel, priceLabel, cartButton]
         
         views.forEach {
             contentView.addSubview($0)
@@ -77,6 +96,36 @@ final class NftCollectionViewCell: UICollectionViewCell {
             $0.translatesAutoresizingMaskIntoConstraints = false
         }
     }
+    
+    private func updateRaitingStackView(raitng: Int) {
+        let yellowStarImage = UIImage(systemName: "star.fill")?.withTintColor(.ypYellowUniversal ?? .yellow).withRenderingMode(.alwaysOriginal)
+        let whiteStarImage = UIImage(systemName: "star.fill")?.withTintColor(.ypLightGrey ?? .lightGray, renderingMode: .alwaysOriginal)
+        
+        guard raitng <= 5 else {
+            return
+        }
+        
+        //проходимся по вьюхам и меняем картинку на желтую звезду пока index меньше рейтинга, но не очень понятно подойдет ли для RTL
+        raitingStackView.arrangedSubviews.enumerated().forEach { index, view in
+            (view as? UIImageView)?.image = index < raitng ? 
+            yellowStarImage :
+            whiteStarImage
+        }
+    }
+    
+    private func updateLikeButtonState(isLiked: Bool) {
+        let whiteHeartImage = UIImage(systemName: "heart.fill")?.withTintColor(.ypWhiteUniversal ?? .white).withRenderingMode(.alwaysOriginal)
+        let redHeartImage = UIImage(systemName: "heart.fill")?.withTintColor(.ypRedUniversal ?? .red).withRenderingMode(.alwaysOriginal)
+        let likeButtonImage = isLiked ? redHeartImage : whiteHeartImage
+    }
+    
+    private func updateCartButtonState(isInCart: Bool) {
+        let addToCartImage = UIImage(named: "AddToCart")
+        let removeFromCartImage = UIImage(named: "removeFromCart")
+        let cartButtonImage = isInCart ? removeFromCartImage : addToCartImage
+        cartButton.setImage(cartButtonImage, for: .normal)
+    }
+    
     
 }
 

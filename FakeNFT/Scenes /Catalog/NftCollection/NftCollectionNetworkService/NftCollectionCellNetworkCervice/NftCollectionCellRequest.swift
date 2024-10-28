87f6ca105ac90b949894e3e7371ed5d784824b09
dreamlogin <dreamlogin@vk.team>
2024-10-29 02:18:39 +0300

    КАТ3 – NftCollectionCellRequest NftCollectionCellService изменены для получения каждой Nft отдельно по id

--- FakeNFT/Scenes /Catalog/NftCollection/NftCollectionNetworkService/NftCollectionCellNetworkCervice/NftCollectionCellRequest.swift
+++ FakeNFT/Scenes /Catalog/NftCollection/NftCollectionNetworkService/NftCollectionCellNetworkCervice/NftCollectionCellRequest.swift
@@ -1,8 +1,9 @@
 import Foundation
 
 struct NftCollectionCellRequest: NetworkRequest {
+    var id: String
     var endpoint: URL? {
-        URL(string: "\(RequestConstants.baseURL)/api/v1/nft")
+        URL(string: "\(RequestConstants.baseURL)/api/v1/nft/\(id)")
     }
     
     var dto: (any Dto)?
--- FakeNFT/Scenes /Catalog/NftCollection/NftCollectionNetworkService/NftCollectionCellNetworkCervice/NftCollectionCellService.swift
+++ FakeNFT/Scenes /Catalog/NftCollection/NftCollectionNetworkService/NftCollectionCellNetworkCervice/NftCollectionCellService.swift
@@ -1,22 +1,23 @@
 import Foundation
 
-typealias NftCollectionCellCompletion = (Result<[NftCollectionCellResultModel], Error>) -> Void
+typealias NftCollectionCellCompletion = (Result<NftCollectionCellResultModel, Error>) -> Void
 
 protocol NftCollectionCellServiceProtocol {
     func loadNftCollection(completion: @escaping NftCollectionCellCompletion)
 }
 
 final class NftCollectionCellService: NftCollectionCellServiceProtocol {
-
+    private let id: String
     private let networkClient: NetworkClient
     
-    init(networkClient: NetworkClient) {
+    init(networkClient: NetworkClient, id: String) {
         self.networkClient = networkClient
+        self.id = id
     }
     
     func loadNftCollection(completion: @escaping NftCollectionCellCompletion) {
-        let request = NftCollectionCellRequest()
-        networkClient.send(request: request, type: [NftCollectionCellResultModel].self, onResponse: completion)
+        let request = NftCollectionCellRequest(id: id)
+        networkClient.send(request: request, type: NftCollectionCellResultModel.self, onResponse: completion)
     }
-
+    
 }

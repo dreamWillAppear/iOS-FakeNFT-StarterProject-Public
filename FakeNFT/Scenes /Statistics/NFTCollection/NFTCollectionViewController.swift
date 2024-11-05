adfb28e3c92db9a96a33105fb44782e017bcf4fb
kostyanTs <114927412+kostyanTs@users.noreply.github.com>
2024-11-05 19:24:24 +0500

    Update NFTCollectionViewController.swift

--- FakeNFT/Scenes /Statistics/NFTCollection/NFTCollectionViewController.swift
+++ FakeNFT/Scenes /Statistics/NFTCollection/NFTCollectionViewController.swift
@@ -57,7 +57,7 @@ final class NFTCollectionViewController: UIViewController, NFTCollectionViewCont
     }
     
     func showProgressHud() {
-        ProgressHUD.show()
+        ProgressHUD.animate()
     }
     
     func hideProgressHud() {
